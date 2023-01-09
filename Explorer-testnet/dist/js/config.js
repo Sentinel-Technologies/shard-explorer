0dc4451a51c10aeb91e21c327a0044bbbfc901b7
madz_miner <95881676+superpythonguy@users.noreply.github.com>
2023-01-09 17:38:23 +0000

    Add files via upload

---
+++ Explorer-testnet/dist/js/config.js
@@ -0,0 +1,9 @@
+const ExplorerConfig = {
+  nodeURL: 'http://madzcoin-58263.portmap.io:58263/',
+  ticker: 'MADZ',
+  blockReward: 10.5,
+  blockTime: 300,
+  maxSupply: 21000000000,
+  decimalPoints: 18
+}
+
