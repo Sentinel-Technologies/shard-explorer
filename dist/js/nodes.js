101cb897d4a8e0eecc0487447a817d97b91b5846
Gustas Savickas <75716744+the-red-eye-studio@users.noreply.github.com>
2023-02-01 19:35:15 +0200

    move files to root ;-;

---
+++ dist/js/nodes.js
@@ -0,0 +1,109 @@
+nodes = [{
+  "name": "Self-hosted node (RedEye)",
+  "protocol": "https",
+  "URL": "78.58.45.205:5000",
+},{
+  "name": "Portmap-Node (MADZ Hacker)",
+  "protocol": "http",
+  "URL": "madzcoin-58263.portmap.io:58263",
+},{
+  "name":"REPL-Node (MADZ Hacker)",
+  "protocol": "https",
+  "URL": "madzcoin-node.superpythonguy.repl.co:443"
+}
+]
+
+$(document).ready(function () {
+  localData.nodeTable = $('#nodes').DataTable({
+    searching: false,
+    info: false,
+    paging: false,
+    lengthMenu: -1,
+    language: {
+      emptyTable: 'No Nodes Found'
+    },
+    columnDefs: [
+      {
+        targets: [0],
+        render: function (data, type, row, meta) {
+          if (type === 'display') {
+            data = '<span title="' + data.address + '">' + data.online + " " + data.name
+          } else if (type === 'sort') {
+            data = data.name
+          }
+          return data
+        }
+      },
+      {
+        targets: [1],
+        render: function (data, type, row, meta) {
+          if (type === 'display') {
+            data = data.host + ':' + data.port + data.ssl + data.cache
+          } else if (type === 'sort') {
+            data = data.host + ':' + data.port
+          }
+          return data
+        }
+      },
+    ],
+    order: [
+      [2, 'dsc']
+    ],
+    autoWidth: false
+  }).columns.adjust().responsive.recalc().draw(false)
+
+
+getAndDrawNodeStats()
+})
+
+async function getAndDrawNodeStats () {
+
+
+      
+
+      localData.nodeTable.clear()
+      for (var i = 0; i < nodes.length; i++) {
+
+
+        var node = nodes[i]
+
+        if (node.protocol == "https") {_https = true} else {_https = false}
+        try {
+          response = await fetch(node.protocol + "://" + node.URL+"/stats");
+          _data = await response.text();
+          online = response.ok
+        } catch {online = false}
+      
+      
+        if (online) {
+          _height = JSON.parse(_data).result.chain.length
+          _txs = JSON.parse(_data).result.coin.transactions
+        } else {
+          _height = 0
+          _txs = 0
+        }
+        
+        if (online == true) {online = "✔"} else {online = "❌"}
+        localData.nodeTable.row.add([
+          {
+            online: online,
+            name: node.name,
+            address: node.protocol + "://" + node.URL
+          },
+          {
+            host: (node.URL).split(':')[0],
+            port: (node.URL).split(':')[1],
+            ssl: _https ? ' <i class="fas fa-user-shield has-trtl-green" title="HTTPS"></i>' : '',
+            cache: false ? ' <i class="fas fa-tachometer-alt has-trtl-green" title="Blockchain Cache"></i>' : ''
+          },
+          _height,
+          _txs
+        ])
+      }
+      localData.nodeTable.draw(false)
+      jdenticon()
+    }
+  setTimeout(() => {
+    getAndDrawNodeStats()
+  }, 15000)
+
