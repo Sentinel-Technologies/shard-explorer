2923eb2ea1ad4420df593708ed8b7a5438caf3d7
SuperPythonguy <supremedoodleryt@outlook.com>
2023-08-27 19:14:42 +0100

    added get block reward

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,9 +1,17 @@
 const ExplorerConfig = {
   nodeURL: 'http://shard-node.duckdns.org:6969/',
   ticker: 'SHRD',
-  blockReward: 10.5,
+  blockReward: 2048,
   blockTime: 300,
   maxSupply: 21000000000,
   decimalPoints: 18
 }
+
+fetch('http://shard-node.duckdns.org:6969/stats')
+  .then(response => response.json())
+  .then(data => {
+    ExplorerConfig.blockReward = data.chain.blockReward;
+    console.log(ExplorerConfig.blockReward);
+  });
+
 
