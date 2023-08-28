5665a9843c300d311a97b60480d90eb24bd55417
SuperPythonguy <supremedoodleryt@outlook.com>
2023-08-28 17:36:24 +0100

    fix

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,16 +1,10 @@
 const ExplorerConfig = {
   nodeURL: 'http://shard-node.duckdns.org:6969/',
   ticker: 'SHRD',
+  blockReward: 100.5,
   blockTime: 300,
   maxSupply: 21000000000,
   decimalPoints: 18
 }
-
-fetch('http://shard-node.duckdns.org:6969/stats')
-  .then(response => response.json())
-  .then(data => {
-    ExplorerConfig.blockReward = data.chain.blockReward;
-    console.log(ExplorerConfig.blockReward);
-  });
 
 
